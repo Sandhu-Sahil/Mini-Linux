55a074c861c2e93493c5566abe8c80758be8d1ff
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-10-04 02:48:15 +0530

    today i have added script of input output on screen, first clear the screen and then add my own text, the cursor handling, and after that the scroll feature to get text at bottom instead of repeating on top from starting of 25x80 array of frame buffer, also a device tracker to get input output at specific device like on screen/terminal or at port that can be separated by this deviceTracker system, updated link.ld beacuse it was not taking input greater than 64 length but now accepting long input didn't understand what effect these change make will understand later, lastly esp register in io.s file understanding is left like how it handles function input

---
+++ day-3/deviceTracker/device.c
@@ -0,0 +1,12 @@
+#include "device.h"
+
+struct device dtable[DEV_COUNT];
+
+int register_device(struct device d){
+	add_dev(d);
+	return 0;	
+}
+
+void dev_write(unsigned int devid, unsigned char *b, unsigned short len){
+	lookup_dev(devid).write(b, len);
+}
---
+++ day-3/kmain.c
@@ -0,0 +1,27 @@
+#include "frameBuffer/fb.h"
+#include "deviceTracker/device.h"
+
+void init(void) {
+	fb_init();
+}
+
+void clear(void){
+	int i;
+	unsigned char s[] = " ";
+	for (i = 0; i < FB_NUM_CELLS; ++i){
+		dev_write(0, s, 1);	
+    }
+}
+
+void kmain(void){
+	unsigned char s[] = "This is MINI Sandhu, mini version of myself Sahilsher Singh Sandhu.";
+	//I am a 3rd year Computer Science student at University of Waterloo. I am a passionate programmer and love to code. I am a quick learner and love to learn new things. I am a team player and love to work in a team. I am a hard worker and love to work hard. I am a good listener and love to listen to people. I am a good speaker and love to speak to people. I am a good leader and love to lead people. I am a good follower and love to follow people. I am a good friend and love to be friends with people. I am a good person and love to be a person. I am a good human and love to be a human. I am a good human being and love to be a human being. I am a good being and love to be a being. I am a good son and love to be a son. I am a good brother and love to be a brother. I am a good friend and love to be a friend. I am a good student and love to be a student. I am a good programmer and love to be a programmer.";
+
+	init();
+
+	clear();
+
+	// fb_change_color(FB_LIGHT_CYAN, FB_RED);
+
+	dev_write(0, s, sizeof(s));
+}
